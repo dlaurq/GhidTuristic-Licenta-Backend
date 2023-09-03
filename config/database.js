1667a719c6abfeb5f898283a4a4ab258ee7da43a
Laurentiu Dutu <qlaurd@gmail.com>
2023-09-03 13:15:02 +0300

    moved from mysql to pg

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 const Sequelize = require('sequelize');
 
-const mysql = require('mysql2');
+//const mysql = require('mysql2');
 
 
 //MYSQL conn
-const connection = mysql.createConnection(process.env.MYSQL_URL);
+//const connection = mysql.createConnection(process.env.MYSQL_URL);
 
 //DB connaction
 const sequelize = new Sequelize({
     username: process.env.MYSQL_USER, 
     password: process.env.MYSQL_PASSWORD,
     database: process.env.DATABASE,
-    dialect: 'mysql',
+    dialect: 'postgres',
     port: process.env.MYSQL_PORT,
     host: process.env.MYSQL_IP,
     define: {
